f890ac67902abeb797b49c860076f7efc0718259
Dominique U <dominiqueu27180@gmail.com>
2020-03-02 10:39:52 +0100

    fichier index.php
    instance d'un objet XMLHTTPRequest avec "vieux code" Microsoft

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <script>
+        let xhr = null;
+        if (windows.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            try {
+                xhr = new ActiveXObject("Msxml2.XMLHTTP");
+            }
+            catch (x) {
+                xhr = new ActiveXObject("Microsoft.XMLHttp");
+
+            }
+        }
+        else{
+            alert('Mettre le navigateur a jour');
+            xhr = false;
+        }
+    </script>
+</body>
+
+</html>
